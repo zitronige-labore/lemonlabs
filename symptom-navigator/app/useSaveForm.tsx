afec9ad068f29c663ee50a64ce034eef24d39f1f
MikailKocan <3023048@stud.hs-mannheim.de>
2026-05-15 11:21:44 +0200

    Save local changes before combining with develop

--- symptom-navigator/app/useSaveForm.tsx
+++ symptom-navigator/app/useSaveForm.tsx
@@ -1,34 +1,34 @@
 "use client"
 
-import { saveFormData} from "./actions"; // form data is being importet here so it will be called in page when function is called
+import { saveFormData } from "./actions"; // form data is being importet here so it will be called in page when function is called
 
-export function useSaveForm(basisData: any, additionalData:any, redFlags: any, selectedMainRegion: any, selectedSubRegion: any, selectedSymptoms: any, symptomText: any, ) {
+export function useSaveForm(basisData: any, additionalData: any, redFlags: any, selectedMainRegion: any, selectedSubRegion: any, selectedSymptoms: any, symptomText: any,) {
 
     // actual function to be used in components
     const handleSaveForm =
-    async () => {
-        // manual calling of saveFormData on klick since the whole page is client side and saveFormData cannot be called with action attribute
-        const formData = new FormData();
-        // assigning the correct data from the form to formData
-        formData.set("age", basisData.age);
-        formData.set("gender", basisData.gender);
-        formData.set("pregnancy", basisData.pregnancy);
-        formData.set("selectedSymptoms", selectedSymptoms);
-        formData.set("symptomText", symptomText.join("|||"));
-        formData.set("selectedMainRegion", selectedMainRegion);
-        formData.set("selectedSubRegion", selectedSubRegion);
-        formData.set("medication", additionalData.medication);
-        formData.set("weight", additionalData.weight);
-        formData.set("height", additionalData.height);
-        formData.set("breastfeeding", additionalData.breastfeeding);
-        formData.set("conditions", additionalData.conditions);
-        formData.set("allergies", additionalData.allergies);
-        formData.set("temperature", additionalData.temperature);
-        formData.set("duration", additionalData.duration);
-        formData.set("worsening", additionalData.worsening);
-        formData.set("extraInfo", additionalData.extraInfo);
-        formData.set("redFlags", redFlags); 
-        await saveFormData(formData); // call the server action to save the form data in the db and set the cookie
+        async () => {
+            // manual calling of saveFormData on klick since the whole page is client side and saveFormData cannot be called with action attribute
+            const formData = new FormData();
+            // assigning the correct data from the form to formData
+            formData.set("age", basisData.age);
+            formData.set("gender", basisData.gender);
+            formData.set("pregnancy", basisData.pregnancy);
+            formData.set("selectedSymptoms", selectedSymptoms);
+            formData.set("symptomText", symptomText.join("|||"));
+            formData.set("selectedMainRegion", selectedMainRegion);
+            formData.set("selectedSubRegion", selectedSubRegion);
+            formData.set("medication", additionalData.medication);
+            formData.set("weight", additionalData.weight);
+            formData.set("height", additionalData.height);
+            formData.set("breastfeeding", additionalData.breastfeeding);
+            formData.set("conditions", additionalData.conditions);
+            formData.set("allergies", additionalData.allergies);
+            formData.set("temperature", additionalData.temperature);
+            formData.set("duration", additionalData.duration);
+            formData.set("worsening", additionalData.worsening);
+            formData.set("extraInfo", additionalData.extraInfo);
+            formData.set("redFlags", redFlags);
+            await saveFormData(formData); // call the server action to save the form data in the db and set the cookie
         }
     return handleSaveForm;
 }
