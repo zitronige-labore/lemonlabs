cc102073551aee3ac91ab49d4b47f1dbbc1bbd9e
Meyneth <frr_gufi@web.de>
2026-06-05 13:14:04 +0200

    works without server connection now but webpack seems unavoidable

--- symptom-navigator/next.config.ts
+++ symptom-navigator/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import withPWA from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: false,   
+  disable: process.env.NODE_ENV === "development",
+})(nextConfig);
